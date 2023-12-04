4d408c9e3522effc673e5c1bf111df57d5b3380a
Ch Ahmad Bhatti <104377785+chahmadbhatti@users.noreply.github.com>
2023-12-04 11:28:07 +0500

    Delete 213047_Lab_8 directory

--- 213047_Lab_8/Ex_2.cs
+++
@@ -1,4 +0,0 @@
-// Assumes that connection is a valid SqlConnection object.
-string queryString = "SELECT CustomerID, CompanyName FROM dbo.Customers";
-SqlDataAdapter adapter = new SqlDataAdapter(queryString, connection);
-DataSet customers = new DataSet(); adapter.Fill(customers, "Customers");
